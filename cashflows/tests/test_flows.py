f84ab11ade66e12de5168ea80727df52a585db7e
rhdzmota <rohdzmota@gmail.com>
2018-01-11 17:27:58 -0600

    add tests for new xirr

--- cashflows/tests/test_flows.py
+++ cashflows/tests/test_flows.py
@@ -5,37 +5,38 @@ import cashflows
 
 class TestFlows(TestCase):
     def test_cash_inflow(self):
-        flow_a = cashflows.FlowConf(
+        flow_a = cashflows.Flow(
             flow_type="inflow",
-            time=5,
             value=100,
-            currency="mxn"
+            time=5
         )
-        self.assertTrue(isinstance(flow_a, cashflows.FlowConf))
+        self.assertTrue(isinstance(flow_a, cashflows.Flow))
 
     def test_cash_outflow(self):
-        flow_a = cashflows.FlowConf(
+        flow_a = cashflows.Flow(
             flow_type="outflow",
-            time=0,
             value=7,
-            currency="mxn"
+            time=0
         )
-        self.assertTrue(isinstance(flow_a, cashflows.FlowConf))
+        self.assertTrue(isinstance(flow_a, cashflows.Flow))
 
     def test_combine_infows(self):
         f_type = "inflow"
+        t_type = "int"
         common_time = 1
         common_currency = "mxn"
-        flow_a = cashflows.FlowConf(
+        flow_a = cashflows.Flow(
             flow_type=f_type,
-            time=common_time,
             value=100,
+            time=common_time,
+            time_type=t_type,
             currency=common_currency
         )
-        flow_b = cashflows.FlowConf(
+        flow_b = cashflows.Flow(
             flow_type=f_type,
-            time=common_time,
             value=50,
+            time=common_time,
+            time_type=t_type,
             currency=common_currency
         )
         flow_a.combine(flow_b)
@@ -43,18 +44,21 @@ class TestFlows(TestCase):
 
     def test_combine_outflows(self):
         f_type = "outflow"
-        common_time = 1
+        t_type = "date"
+        common_time = "01/01/2018"
         common_currency = "mxn"
-        flow_a = cashflows.FlowConf(
+        flow_a = cashflows.Flow(
             flow_type=f_type,
-            time=common_time,
             value=100,
+            time=common_time,
+            time_type=t_type,
             currency=common_currency
         )
-        flow_b = cashflows.FlowConf(
+        flow_b = cashflows.Flow(
             flow_type=f_type,
-            time=common_time,
             value=50,
+            time=common_time,
+            time_type=t_type,
             currency=common_currency
         )
         flow_a.combine(flow_b)
@@ -63,57 +67,65 @@ class TestFlows(TestCase):
     def test_combine_opposites_no_flip(self):
         common_time = 1
         common_currency = "mxn"
-        flow_a = cashflows.FlowConf(
+        flow_a = cashflows.Flow(
             flow_type="inflow",
-            time=common_time,
             value=100,
+            time=common_time,
             currency=common_currency
         )
-        flow_b = cashflows.FlowConf(
+        flow_b = cashflows.Flow(
             flow_type="outflow",
-            time=common_time,
             value=50,
+            time=common_time,
             currency=common_currency
         )
         flow_a.combine(flow_b)
         self.assertTrue((flow_a.value == 50) and (flow_a.type != flow_b.type))
 
     def test_combine_opposites_with_flip(self):
-        flow_a = cashflows.FlowConf(
+        flow_a = cashflows.Flow(
             flow_type="inflow",
-            time=1,
             value=100,
+            time=1,
             currency="mxn"
         )
-        flow_b = cashflows.FlowConf(
+        flow_b = cashflows.Flow(
             flow_type="outflow",
-            time=1,
             value=50,
+            time=1,
             currency="mxn"
         )
         flow_b.combine(flow_a)
         self.assertTrue((flow_b.value == 50) and (flow_a.type == flow_b.type))
 
     def test_cashflow(self):
-        cash_flows = cashflows.CashFlow()
-        cash_flows.add("outflow", 0, 100, "mxn")
-        cash_flows.add("inflow", 2, 50, "mxn")
-        cash_flows.add("inflow", 4, 80, "mxn")
-        cash_flows.add("outflow", 4, 10, "mxn")
+        cash_flows = cashflows.CashFlow("int")
+        cash_flows.add(flow_type="outflow", value=100, time=0)
+        cash_flows.add(flow_type="inflow", value=50, time=2)
+        cash_flows.add(flow_type="inflow", value=80, time=4)
+        cash_flows.add(flow_type="outflow", value=10, time=4)
         self.assertTrue(isinstance(cash_flows, cashflows.CashFlow))
 
     def test_profitability(self):
-        cash_flows = cashflows.CashFlow()
-        cash_flows.add("outflow", 0, 100, "mxn")
-        cash_flows.add("inflow", 2, 50, "mxn")
-        cash_flows.add("inflow", 4, 80, "mxn")
-        cash_flows.add("outflow", 4, 10, "mxn")
+        cash_flows = cashflows.CashFlow("int")
+        cash_flows.add(flow_type="outflow",  value=100, time=0)
+        cash_flows.add(flow_type="inflow", value=50, time=2)
+        cash_flows.add(flow_type="inflow", value=80, time=4)
+        cash_flows.add(flow_type="outflow",  value=10, time=4)
         self.assertTrue(cash_flows.get_profitability() == 1.2)
 
     def test_irr(self):
-        cash_flows = cashflows.CashFlow()
-        cash_flows.add("outflow", 0, 100, "mxn")
-        cash_flows.add("inflow", 2, 50, "mxn")
-        cash_flows.add("inflow", 4, 80, "mxn")
-        cash_flows.add("outflow", 4, 10, "mxn")
+        cash_flows = cashflows.CashFlow("int")
+        cash_flows.add(flow_type="outflow",  value=100, time=0)
+        cash_flows.add(flow_type="inflow", value=50, time=2)
+        cash_flows.add(flow_type="inflow", value=80, time=4)
+        cash_flows.add(flow_type="outflow",  value=10, time=4)
         self.assertTrue(cash_flows.get_irr() == 5.981718)
+
+    def test_xirr(self):
+        cash_flows = cashflows.CashFlow("date")
+        cash_flows.add(flow_type="outflow", value=100, time="01/01/2018")
+        cash_flows.add(flow_type="inflow", value=50, time="01/01/2020")
+        cash_flows.add(flow_type="inflow", value=80, time="01/01/2022")
+        cash_flows.add(flow_type="outflow", value=10, time="01/01/2022")
+        self.assertTrue(cash_flows.get_irr() == 5.9787100000000004)
